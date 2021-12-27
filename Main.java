95d585541df0f4447c6fe6abb3a52bda1a39db3d
CxiCxi <95742168+CxiCxi@users.noreply.github.com>
2021-12-27 20:40:17 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
-import view.GameFrame;
-
 import javax.swing.*;
 
-public class Main {
+public class Main extends JFrame {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-
-            GameFrame mainFrame = new GameFrame(800);
-            mainFrame.setVisible(true);
-            mainFrame.
+            initialFrame InitialFrame = new initialFrame(800);
+            JLabel ab = new JLabel() ;
+            JPanel contentPane = new JPanel();
+            InitialFrame .setContentPane(contentPane);
+            contentPane .add(ab) ;
+            InitialFrame .setVisible(true);
         });
     }
 }
